29381513ef3ba3e571cf4c2aef80eb45964e6f5e
akashsingh79036 <akashsingh79036@gmail.com>
2026-08-02 20:52:54 +0530

    add dagshub token in my gitub secret1

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -22,7 +22,7 @@ os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 dagshub_url = "https://dagshub.com"
 repo_owner = "akashsingh79036"
 repo_name = "model_monitoring"
-
+#add dagshub token inside github secret"
 # Set up MLflow tracking URI
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
